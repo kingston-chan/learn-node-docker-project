2fa6b56c6d706e3197f9968ec759873b22763ead
Chan <Kingston.Chan@tabcorp.com.au>
2023-04-24 17:03:27 +1000

    Simple get request to get all users

--- backend/src/controllers/controller.js
+++ backend/src/controllers/controller.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
 const User = require('../models/model');
+
+exports.getUsers = async (req, res, next) => {
+    try {
+        return await User.find({});
+    } catch (err) {
+        next(err);
+    }
+}
