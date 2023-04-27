ee64ae388d900be49ec0a3b1b58064dc9423c8be
Chan <Kingston.Chan@tabcorp.com.au>
2023-04-27 16:14:11 +1000

    Removed files and reinstalled and uninstalled dependencies

--- backend/src/routes/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/controller');
-
-router.get('/users', controller.getUsers);
-router.post('/user', controller.postUser);
-
-module.exports = router;
